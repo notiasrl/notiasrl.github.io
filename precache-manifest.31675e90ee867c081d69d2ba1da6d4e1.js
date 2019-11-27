c19fdcd35d959cd444d40fc636774e17d8c8a548
Luis Zayas <zayaslui@gmail.com>
2019-11-27 01:31:52 -0300

    update

---
+++ precache-manifest.31675e90ee867c081d69d2ba1da6d4e1.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "946f41fc8bd472a481204bed438390ac",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8c73c3b23a9d7bee943d",
+    "url": "/static/css/main.da005ed0.chunk.css"
+  },
+  {
+    "revision": "ce1f8a0588bf88c4d230",
+    "url": "/static/js/2.415bf2f3.chunk.js"
+  },
+  {
+    "revision": "8c73c3b23a9d7bee943d",
+    "url": "/static/js/main.4d1b76f4.chunk.js"
+  },
+  {
+    "revision": "add4100c9c4aa7e9dc95",
+    "url": "/static/js/runtime~main.0ccb7db5.js"
+  },
+  {
+    "revision": "bc1d0ac9f247a7434395612449bbe5dc",
+    "url": "/static/media/almacenaje.bc1d0ac9.jpg"
+  },
+  {
+    "revision": "19f3394f7d9a365fd84de98745a6bede",
+    "url": "/static/media/banner2.19f3394f.jpg"
+  },
+  {
+    "revision": "dc9db7e3009f28027669c780a405d0a0",
+    "url": "/static/media/bg.dc9db7e3.jpg"
+  },
+  {
+    "revision": "e52419719dd0c4c8b172136f0b625008",
+    "url": "/static/media/bg.e5241971.jpg"
+  },
+  {
+    "revision": "e45b045878f5d710025b0edf2b116664",
+    "url": "/static/media/bg1.e45b0458.jpg"
+  },
+  {
+    "revision": "047482699d9080626fda39fee07fcdab",
+    "url": "/static/media/bg2.04748269.jpg"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "ade34a38d10db85292f7d74bf22a855c",
+    "url": "/static/media/bg3.ade34a38.jpg"
+  },
+  {
+    "revision": "e76de24b6d8ab5e920bb4d49241ad15f",
+    "url": "/static/media/bg3.e76de24b.jpg"
+  },
+  {
+    "revision": "b08234a2b7e8e4fb2e3c4e713252021c",
+    "url": "/static/media/bg7.b08234a2.jpg"
+  },
+  {
+    "revision": "0f5d5fb14ef1795fe6e13c1fb15a6984",
+    "url": "/static/media/container.0f5d5fb1.jpg"
+  },
+  {
+    "revision": "ac4f0b48ff6e228033308a915a68a968",
+    "url": "/static/media/g1.ac4f0b48.jpg"
+  },
+  {
+    "revision": "4096cd6d378eadc98f2158ccc67ab79e",
+    "url": "/static/media/g2.4096cd6d.jpg"
+  },
+  {
+    "revision": "0ef48dde8df77da338fa28926f604718",
+    "url": "/static/media/g3.0ef48dde.jpg"
+  },
+  {
+    "revision": "48fca673be9fa095ea78ec3b36e72cea",
+    "url": "/static/media/g4.48fca673.jpg"
+  },
+  {
+    "revision": "51b76742af9064e885d802979c7e8ad0",
+    "url": "/static/media/g5.51b76742.jpg"
+  },
+  {
+    "revision": "57ec00b9edd2619de8f58ba95e30abf6",
+    "url": "/static/media/g6.57ec00b9.jpg"
+  },
+  {
+    "revision": "f1f7fa31ea0c16ba54a0c335213ea33a",
+    "url": "/static/media/g7.f1f7fa31.jpg"
+  },
+  {
+    "revision": "a1fd7fd18545a3b117fe37840b15aeb3",
+    "url": "/static/media/g8.a1fd7fd1.jpg"
+  },
+  {
+    "revision": "ff43722352812dc01a4b228b949b183f",
+    "url": "/static/media/g9.ff437223.jpg"
+  },
+  {
+    "revision": "a54e3d371521f721d605ab193b720502",
+    "url": "/static/media/logo.a54e3d37.svg"
+  },
+  {
+    "revision": "97b4d814fcc21ad5bcdecd0e6c1898b8",
+    "url": "/static/media/maquina.97b4d814.jpg"
+  },
+  {
+    "revision": "78e1d65d39453cc4eddd390a765d5341",
+    "url": "/static/media/mision.78e1d65d.jpg"
+  },
+  {
+    "revision": "9567441049d30103de825473b886535e",
+    "url": "/static/media/notia.95674410.png"
+  },
+  {
+    "revision": "49bc4f617fa0b647fed5c2f9eede15ad",
+    "url": "/static/media/soja.49bc4f61.jpg"
+  }
+]);
